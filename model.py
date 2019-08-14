846f755accc01bdd08917f6b32505b7bf8de6d0a
zmarnur <32554788+zmarnur@users.noreply.github.com>
2019-08-14 10:02:13 +0200

    Create model.py

---
+++ model.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+
+class Contact(models.Model):
+  name = models.CharField(max_length=200)
+  second_name = models.CharField(max_length=200) 
+  email = models.EmailField(unique=true)
+  phone = models.CharField(max_length=15)
+  add_date = models.DateTimeField(
+        default = timezone.now)
+           
+           
+ def __str__(self):
+    return self.title
+
