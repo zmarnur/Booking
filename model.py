f6a4c96237e9ad87093fe2e93a216d85abfe8466
zmarnur <32554788+zmarnur@users.noreply.github.com>
2019-08-14 10:05:19 +0200

    Update model.py

--- model.py
+++ model.py
@@ -6,6 +6,7 @@ class Contact(models.Model):
   second_name = models.CharField(max_length=200) 
   email = models.EmailField(unique=true)
   phone = models.CharField(max_length=15)
+  note = models.TextField(max_length=300)
   add_date = models.DateTimeField(
         default = timezone.now)
            
